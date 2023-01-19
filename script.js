715ab1f2be81663169be901a7adc311fa3012314
aidenfranco4 <102084871+aidenfranco4@users.noreply.github.com>
2023-01-19 17:05:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const showTime = () => {
   let hour = time.getHours();
   let min = time.getMinutes();
   let sec = time.getSeconds();
-  am_pm = "AM";
+  let am_pm = "AM";
   
   if(hour > 12){
     hour -= 12;
